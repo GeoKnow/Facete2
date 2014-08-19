184fc3435f5ff452631f848aab4017be6c3865c8
cstadler <cstadler@informatik.uni-leipzig.de>
2014-08-19 23:16:04 +0200

    cleaned up index.html

---
+++ facete2-webapp/src/main/webapp/scripts/js/app.js
@@ -0,0 +1,29 @@
+angular.module('Facete2', ['ui.router', 'ui.bootstrap', 'ui.jassa', 'ngGrid', 'ngTable', 'ui.jassa.openlayers', 'ngSanitize'], function($rootScopeProvider) {
+    $rootScopeProvider.digestTtl(10);
+})
+
+.config([function() {
+    // Setup drop down menu
+    jQuery('.dropdown-toggle').dropdown();
+
+    // Fix input element click problem
+    jQuery('.dropdown input, .dropdown label').click(function(e) {
+      e.stopPropagation();
+    });
+}])
+
+.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise("/home");
+
+
+    //
+    // Now set up the states
+    $stateProvider
+        .state('home', {
+            url: '/home',
+            templateUrl: 'partials/home.html',
+           // controller: 'SearchCtrl'
+        })
+        ;
+
+}]);
