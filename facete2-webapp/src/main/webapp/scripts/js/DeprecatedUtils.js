da447ddd73f0d7a8f7ce948f1371280602308bc0
cstadler <cstadler@informatik.uni-leipzig.de>
2015-02-18 11:03:52 +0100

    Fixed export again

--- facete2-webapp/src/main/webapp/scripts/js/DeprecatedUtils.js
+++ facete2-webapp/src/main/webapp/scripts/js/DeprecatedUtils.js
@@ -1,25 +1,4 @@
-    /**
-     * Index the map of a spring batch object
-     * this is for spring batch ~2.2
-     */
-    var indexBatchMapOld = function(map) {
-        var result;
-        var entry = map ? map.entry : null;
 
-        if(!entry) {
-            result = {};
-        }
-        else if(_(entry).isArray()) {
-            result = _(entry).indexBy('string');
-        }
-        else {
-            result = {};
-            var key = entry['string'];
-            result[key] = entry;
-        }
-
-        return result;
-    };
 
     var applyRootScope = function(scope) {
         var rootScope = scope.$root;
--- facete2-webapp/src/main/webapp/scripts/js/home.js
+++ facete2-webapp/src/main/webapp/scripts/js/home.js
@@ -1003,6 +1003,29 @@ angular.module('Facete2')
 
 
 
+    /**
+     * Index the map of a spring batch object
+     * this is for spring batch ~2.2
+     */
+    var indexBatchMapOld = function(map) {
+        var result;
+        var entry = map ? map.entry : null;
+
+        if(!entry) {
+            result = {};
+        }
+        else if(_(entry).isArray()) {
+            result = _(entry).indexBy('string');
+        }
+        else {
+            result = {};
+            var key = entry['string'];
+            result[key] = entry;
+        }
+
+        return result;
+    };
+
     var indexBatchMap = function(entries) {
         var result = {};
         entries.forEach(function(entry) {
