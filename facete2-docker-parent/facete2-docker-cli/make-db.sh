e605becdcec44ac41b4b33d2518ccb969dd21310
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2019-02-13 12:57:49 +0100

    at least a jetty is running...

--- facete2-docker-parent/facete2-docker-cli/make-db.sh
+++ facete2-docker-parent/facete2-docker-cli/make-db.sh
@@ -7,4 +7,6 @@ EOSQL
 #CREATE USER postgres;
 #GRANT ALL PRIVILEGES ON DATABASE facete2tomcatcommon TO facete2tomcatcommon;
 
+su postgres -c "psql -d facete2tomcatcommon -f /tmp/pgsql"
+
 service postgresql stop
