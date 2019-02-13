57bebad5fcd0a334a43805c663df4bdd80c35ee4
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2019-02-13 13:08:27 +0100

    initially working docker setup

--- facete2-docker-parent/facete2-docker-cli/make-db.sh
+++ facete2-docker-parent/facete2-docker-cli/make-db.sh
@@ -1,6 +1,7 @@
 service postgresql start
 export PGUSER=postgres
 su postgres -c "psql <<- EOSQL
+    ALTER ROLE postgres WITH PASSWORD 'postgres';
     CREATE DATABASE facete2tomcatcommon;
 EOSQL
 "
